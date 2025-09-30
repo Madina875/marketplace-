6e626a5903166957cb6de2c0698bdbe9d9cca145
madina875 <evaevasmq@gmail.com>
2025-09-30 22:56:50 +0500

    hmw

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -8,17 +8,17 @@ import { FiInstagram } from "react-icons/fi";
 export const Footer = memo(() => {
   return (
     <footer className="h-[330px] w-full bg-[#2B2B2B]">
-      <div className="container flex justify-between text-[#CCCCCC]">
-        <div>
-          <div className="text-white text-2xl  gap-2 flex items-center">
+      <div className="container flex justify-between pt-[40px] text-[#CCCCCC]">
+        <div className="w-70">
+          <div className="text-white text-2xl mb-6 gap-2 flex items-center">
             <RiStoreLine />
             <p>NFT Marketplace</p>
           </div>
-          <p className="text-[#CCCCCC]">
+          <p className="text-[#CCCCCC] w-55 mb-4">
             NFT marketplace UI created with Anima for Figma.
           </p>
-          <p className="text-[#CCCCCC]">Join our community</p>
-          <div className="text-[#CCCCCC] text-2xl flex items-center">
+          <p className="text-[#CCCCCC] mb-2">Join our community</p>
+          <div className="text-[#CCCCCC] text-2xl gap-2 p-1 flex items-center">
             <SiProbot />
             <LuYoutube />
             <FiTwitter />
@@ -26,19 +26,25 @@ export const Footer = memo(() => {
           </div>
         </div>
         <div>
-          <p className="text-white mb-4 text-2xl">Explore</p>
+          <p className="text-white mb-6 text-2xl">Explore</p>
           <ul className="grid gap-4">
             <li>Marketplace</li>
             <li>Rankings</li>
             <li>Connect a wallet</li>
           </ul>
         </div>
-        <div className="w-105">
-          <p className="text-white text-[20px] mb-5">Join our weekly digest</p>
+        <div className="w-75">
+          <p className="text-white text-[20px] mb-6">Join our weekly digest</p>
           <p>Get exclusive promotions & updates straight to your inbox.</p>
-          <div>
-            <input type="text" placeholder="Enter your email here" />
-            <button>Subscribe</button>
+          <div className="h-[50px] bg-white rounded-[18px] my-5 place-items-center flex">
+            <input
+              type="text"
+              className=" indent-4 placeholder-black"
+              placeholder="Enter your email here"
+            />
+            <button className="bg-purple-700 w-full h-full rounded-[18px]">
+              Subscribe
+            </button>
           </div>
         </div>
       </div>
